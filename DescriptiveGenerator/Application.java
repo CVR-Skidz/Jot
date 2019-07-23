7a88d1916f6fb9c86d0dcfc5d155a9dc6cd29f20
CVR-Skidz <callum.cvr@gmail.com>
2019-07-24 09:17:59 +1200

    Application : working name gen

--- DescriptiveGenerator/Application.java
+++ DescriptiveGenerator/Application.java
@@ -2,19 +2,35 @@
 public class Application {
 	public static void main(String[] args) {
 		boolean genNoun = false;
-		int nouns = 0;
 		int adjectives = 0;
+		String result = new String();
 
 		if (args.length == 1) {
 			genNoun = true;
-			adjectives = Integer.parseInt(args[0]);
+			adjectives = Integer.parseInt(args[0]) - 1;
 		} else if (args.length != 2) {
 			System.err.println("Unknown arguments, use: [number] [noun]<optional>");
 			return;
 		}
-	
+
+		// if 2 arguments passed
+		if (!genNoun) {
+			adjectives = Integer.parseInt(args[0]);
+		}
+
 		Dictionary dict = new Dictionary("adjectives.txt");
+
+		// adjectives
+		result = result.concat(dict.genWords(adjectives));
+
+		// nouns
+		if (genNoun) {
+			dict = new Dictionary("nouns.txt");
+			result = result.concat(dict.genWords(1));
+		} else {
+			result = result.concat(args[1]);
+		}
 		
-		System.out.println(dict.genWords(Integer.parseInt(args[0])));
+		System.out.println("\n--- Name: " + result);
 	}
 }
